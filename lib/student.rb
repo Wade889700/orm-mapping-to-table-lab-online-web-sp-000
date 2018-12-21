51ca4844f26a85726db8d0b3194da282e0a173ac
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-21 07:06:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,16 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initizlize(name:, grade:)
+  def initialize(name, grade, id = nil)
+    @id = id 
     @name = name
     @grade = grade
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def initialize(name, grade, id=nil) 
+     @name = name
+     @grade = grade
+     @id = id
+   end
 end
