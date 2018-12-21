8b8a49f5e920a180a1f687914904c1f09027d18c
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-21 07:08:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def self.create_table
-
+    sql <<-SQL 
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      type TEXT
+    )
   end
 end
